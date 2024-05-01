12a5c631d6e534d35fb1741b9068301e2da669a6
Harsh Khavale <harshkhavale1102@gmail.com>
2024-05-01 05:10:40 -0700

    ready to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,124 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from "react";
+import StarIcon from "@mui/icons-material/Star";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import CloseIcon from "@mui/icons-material/Close";
+
+interface PraiseOptionProps {
+  text: string;
+  handleClick: (text: string) => void;
+  isPraised: boolean;
+}
+
+const PraiseOption: React.FC<PraiseOptionProps> = ({ text, handleClick, isPraised }) => {
+  return (
+    <p
+      className={`bg-gray-200 cursor-pointer rounded-3xl p-2 flex justify-center font-bold text-center text-xs items-center ${
+        isPraised ? "bg-green-300 border-2 border-green-500" : ""
+      }`}
+      onClick={() => handleClick(text)}
+    >
+      {text}
+    </p>
+  );
+};
 
 export default function Home() {
+  const [modal, setModal] = useState(true);
+  const [safety, setSafety] = useState(0);
+  const [communication, setCommunication] = useState(0);
+  const [recommendation, setRecommendation] = useState(true);
+  const [praise, setPraise] = useState<string[]>([]);
+
+  const handleToggle = () => {
+    setModal(!modal);
+  };
+
+  const handleSafetyChange = (rating: number) => {
+    setSafety(rating);
+  };
+
+  const handleCommunicationChange = (rating: number) => {
+    setCommunication(rating);
+  };
+
+  const handleRecommendationChange = (value: boolean) => {
+    setRecommendation(value);
+  };
+
+  const handlePraiseClick = (selectedPraise: string) => {
+    if (praise.includes(selectedPraise)) {
+      setPraise(praise.filter((item) => item !== selectedPraise));
+    } else {
+      setPraise([...praise, selectedPraise]);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex items-center justify-center p-8">
+      {modal && (
+        <form className="flex flex-col gap-6">
+          <CloseIcon onClick={handleToggle} className="cursor-pointer text-right absolute right-8 top-4" />
+          <p className="font-bold text-xl">Leave a review</p>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className="safe flex flex-col gap-2">
+            <p className="font-bold">Safety</p>
+            <span className="text-sm">How safe did you feel with Trausti?</span>
+            <div>
+              {[...Array(5)].map((_, index) => (
+                <StarIcon
+                  key={index}
+                  className={`cursor-pointer ${index < safety ? "text-yellow-400" : "text-gray-400"}`}
+                  onClick={() => handleSafetyChange(index + 1)}
+                />
+              ))}
+            </div>
+          </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="communication flex flex-col gap-2">
+            <p className="font-bold">Communication</p>
+            <span className="text-sm">How easily did you communicate with Trausti?</span>
+            <div>
+              {[...Array(5)].map((_, index) => (
+                <StarIcon
+                  key={index}
+                  className={`cursor-pointer ${index < communication ? "text-yellow-400" : "text-gray-400"}`}
+                  onClick={() => handleCommunicationChange(index + 1)}
+                />
+              ))}
+            </div>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="recommendation flex flex-col gap-2">
+            <p className="font-bold">Would you recommend Trausti?</p>
+            <span className=" text-sm">Would you recommend Trausti to others?.</span>
+            <div className="flex gap-4">
+              <div className="flex gap-2" onClick={() => handleRecommendationChange(false)}>
+                <ThumbDownIcon className={`cursor-pointer ${recommendation ? "text-gray-400" : "text-green-400"}`} />
+                <p>No</p>
+              </div>
+              <div className="flex gap-2" onClick={() => handleRecommendationChange(true)}>
+                <ThumbUpIcon className={`cursor-pointer ${!recommendation ? "text-gray-400" : "text-green-400"}`} />
+                <p>Yes</p>
+              </div>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className="praise flex flex-col gap-2">
+            <p className="font-bold">Praise</p>
+            <span className=" text-sm">What did you like most about Trausti?</span>
+            <div className="flex gap-4">
+              <PraiseOption text="Service" handleClick={handlePraiseClick} isPraised={praise.includes("Service")} />
+              <PraiseOption text="Quality" handleClick={handlePraiseClick} isPraised={praise.includes("Quality")} />
+              <PraiseOption text="Great Experience" handleClick={handlePraiseClick} isPraised={praise.includes("Great Experience")} />
+            </div>
+          </div>
+          <div className=" flex w-full my-4">
+            <button type='button' onClick={()=>handleToggle} className=" border-2 w-full font-bold p-1 border-green-500 bg-green-300">submit</button>
+          </div>
+        </form>
+      )}
     </main>
   );
 }
